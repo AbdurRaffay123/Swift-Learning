5f68693f1c9c94741345c95992c743e085a1ca58
ABDUR Raffay Iftakhar DURRANI <137611267+AbdurRaffay123@users.noreply.github.com>
2025-07-29 17:33:34 +0500

    Add files via upload

---
+++ Book.swift
@@ -0,0 +1,11 @@
+//
+//  Book.swift
+//  BookWorm
+//
+//  Created by hst on 29/07/2025.
+//
+
+import Foundation
+import CoreData
+
+
---
+++ BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by hst on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
